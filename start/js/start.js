3b95a83461b72a2de33f66fedea9caa11a9bafe5
leeboa2005 <leeboa2005@naver.com>
2023-08-02 20:49:33 +0900

    updated qna content

--- start/js/start.js
+++ start/js/start.js
@@ -3,19 +3,46 @@
 const main = document.querySelector("#main");
 const result = document.querySelector("#result");
 const qna = document.querySelector("#qna");
-
+begin
 const endPoint = 12;
 const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-function begin() {
+function addAnswer(answerText, qIdx) {
+  const a = document.querySelector('.answerBox');
+  let answer = document.createElement('button');
+  answer.classList.add('answerList');
+  a.appendChild(answer);
+  answer.innerHTML = answerText;
+
+  answer.addEventListener("click", function () {
+    let children = document.querySelectorAll('.answerList');
+    for (let i = 0; i < children.length; i++) {
+      children[i].disabled = true;
+      children[i].style.display = 'none';
+    }
+    goNext(++qIdx);
+  }, false);
+};
+
+function goNext(qIdx) { // qnaList안에 속성을 전부 가져옴 
+  const q = document.querySelector('.qBox');
+  q.innerHTML = qnaList[qIdx].q;
+  for (let i in qnaList[qIdx].a) {
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+  }
+}
+
+function begin() { // 화면 전환 함수
   main.style.webkitAnimate = "fadeOut 1s";
   main.style.animate = "fadeOut 1s";
   setTimeout(() => {
     qna.style.animate = "fadeIn 1s";
     qna.style.webkitAnimate = "fadeIn 1s";
     setTimeout(() => {
-        main.style.display = "none";
-        qna.style.display = "block";
-    }, 500);
-  }, 500);
+      main.style.display = "none";
+      qna.style.display = "block";
+    }, 450);
+    let qIdx = 0; // 질문이 12번 출력되야하기 때문에 피라미터값으로 선언한다.
+    goNext(qIdx);
+  }, 450);
 }
